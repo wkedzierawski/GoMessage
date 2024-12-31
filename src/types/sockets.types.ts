1aebf2d1e09d9edd83bdac59a0a8b00af7c3a848
victorowsky <victorowskyy@gmail.com>
2024-12-31 14:39:22 +0100

    file types

--- src/types/sockets.types.ts
+++ src/types/sockets.types.ts
@@ -3,18 +3,21 @@ export enum SocketEvent {
 	sendMessage = "sendMessage",
 }
 
+export type SocketFile = { content: File; type: string };
+
 export type SocketPayload = {
 	[SocketEvent.onMessage]: {
 		messageId: string;
 		date: string;
 		message: string;
 		chatId: string;
+		files: SocketFile[];
 		from: string;
 	};
 	[SocketEvent.sendMessage]: {
 		date: string;
 		message: string;
 		chatId: string;
-		files: File[];
+		files: SocketFile[];
 	};
 };
